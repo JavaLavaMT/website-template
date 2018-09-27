8c006a574ad010622c4f48443e5df18d9721eae9
Jeremy Cherer <chererjeremy@gmail.com>
2018-09-27 09:05:00 -0600

    lintting

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -2,12 +2,13 @@ import Component from '@ember/component';
 import $ from 'jquery';
 
 export default Component.extend({
-  actions:{
-  call () {
-    $('.ui.dropdown')
-    .dropdown({
-    action: 'select'
-  });
-}
-}
+
+  actions: {
+    call() {
+      $('.ui.dropdown')
+        .dropdown({
+          action: 'select'
+        });
+    }
+  }
 });
